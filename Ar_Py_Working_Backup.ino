fdffb2ec4d22d9a148903aec8fba4b36ed8ef5aa
Mucahit Kayadan <muhammetkayadan@hotmail.com>
2018-08-23 09:56:38 +0300

    Add files via upload

--- Ar_Py_Working_Backup.ino
+++ Ar_Py_Working_Backup.ino
@@ -2,35 +2,58 @@
 char starter_byte     = 0XAA;
 char source_byte      = 0X01;
 char destination_byte = 0X02;
-char command_byte     = 0XBB;
-char zero_byte        = 0X00;
-char pack[12];
+char command_byte     ;
+char ack_byte        = 0X01;
 int ndx = 0;
-
-                
+char pack[12];
+boolean starterReceived;
+bool checkStarter(char myByte){
+  if (char(myByte) == char(0XAA)) return true;
+  }
+bool checkSource(char myByte){
+  if (char(myByte) == char(0X01)) return true;
+  } 
+bool checkDest(char myByte){
+  if (char(myByte) == char(0X02)) return true; //It will work just for max2828
+  }   
+bool checkCommand(char myByte){
+  if (char(myByte) >= char(0X01) && myByte <= char(0X07)) return true;
+  } 
+      
 void setup() {
 Serial.begin(9600);
 while(!Serial){;}
 }
 
 void loop() { 
-       char Ack[12] = {starter_byte, source_byte, destination_byte, command_byte,
-                zero_byte, zero_byte, zero_byte, zero_byte, 
-                zero_byte, zero_byte, zero_byte, zero_byte};
-       while(Serial.available() ){  
+     
+       while(Serial.available()){
+        /*for(int i = 0; i<=11 ; i++){
         char rb = Serial.read();
-        pack[ndx] = rb;        
-        if(pack[0] == char(0XAA)) {
-          source_byte = char(pack[2]);
-          destination_byte = char(pack[1]);
-          Serial.write(pack[ndx]);
-          ndx++;}
-        if(ndx == 12){
-            for(int i = 0; i < 12; i++)
+        pack[i] = rb; } */
+        char rb = Serial.read();
+        pack[ndx] = rb;
+        if(checkStarter(pack[0])){        
+           ndx++;}
+        starter_byte     = pack[0];
+        source_byte      = pack[2];
+        destination_byte = pack[1];
+        command_byte     = pack[3];
+        ack_byte         = char(0X01);
+        char Ack[5] = {starter_byte, source_byte, destination_byte, command_byte, ack_byte};
+        if(ndx == 12 && checkStarter(pack[0]) && checkSource(pack[1]) && checkDest(pack[2]) && checkCommand(pack[3]) ){
+            for(int i = 0; i < 5; i++)
             Serial.write(char(Ack[i])); 
             ndx=0;
-          } 
-             
+          }
+          else if(ndx == 12 ){
+             ack_byte = char(0X00);
+             char Ack[5] = {starter_byte, source_byte, destination_byte, command_byte, ack_byte};
+             for(int i = 0; i < 5; i++)
+             Serial.write(char(Ack[i])); 
+             ndx=0;
+          }
+                   
         }
               
   }
